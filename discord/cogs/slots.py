5880dd35d81739d2ccf165046bf6333382f5d3f0
bilionierbyaek <aekaekapipoo@outlook.com>
2024-07-11 16:30:53 +0000

    commit

--- discord/cogs/slots.py
+++ discord/cogs/slots.py
@@ -3,7 +3,7 @@ import os
 import random
 
 import discord
-from discord.ext import commands
+from discord.ext import commands # type: ignore
 from modules.economy import Economy
 from modules.helpers import *
 from PIL import Image
